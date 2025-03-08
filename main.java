54d638ebbfbb2fd124da0f8e238f8ce91a79d9dd
mdisfahan <isfahan9353@gmail.com>
2025-03-08 13:28:01 +0530

    initial commit

--- main.java
+++ main.java
@@ -5,6 +5,7 @@ public class main {
         System.out.println("This is java program");
         System.out.println("output from  java program");
         System.out.println(new Date());
+        System.out.println("hello");
         
     }
 }
